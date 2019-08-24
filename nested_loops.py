3c677e736ce5f01a789d7bdf38cc835facf9d67a
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 09:50:15 +0500

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -15,6 +15,20 @@
 # your code goes here
 
 ################################### Q2 ###################################
+# Write a program to that asks the User to enter the number of rows, and #
+# and then prints this pattern on the all the rows:                      #
+#   1. 1                                                                 #
+#   2. 1 2                                                               #
+#   3. 1 2 3                                                             #                      
+#   4. 1 2 3 4                                                           #
+# (This would be the result if the User entered 4, since it has 4 rows)  # 
+# Each row should have numbers starting from 1 that are less than or     #
+# equal to the row number.                                               #
+##########################################################################
+
+# your code goes here
+
+################################### Q3 ###################################
 # Write a program to print the following pattern, using a nested         #
 # for loop. Use the range function.                                      #
 #                                                                        #
