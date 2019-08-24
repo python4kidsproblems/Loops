dbc11fa538725e463241428f9653572990a09cb8
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 09:41:10 +0500

    Add files via upload

---
+++ nested_loops.py
@@ -0,0 +1,32 @@
+# Nested Loops Practice Problems
+
+# How to use this file:
+# Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# Write a program to print the following using nested for loops:         #
+#   1. 1 2 3 4 5                                                         #
+#   2. 1 2 3 4 5                                                         #
+#   3. 1 2 3 4 5                                                         #
+#   4. 1 2 3 4 5                                                         # 
+#   5. 1 2 3 4 5                                                         #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Write a program to print the following pattern, using a nested         #
+# for loop. Use the range function.                                      #
+#                                                                        #
+#  *                                                                     #
+#  * *                                                                   #
+#  * * *                                                                 #
+#  * * * *                                                               #
+#  * * * * *                                                             #
+#  * * * *                                                               #
+#  * * *                                                                 #
+#  * *                                                                   #          
+#  *                                                                     #
+##########################################################################
+
+# your code goes here
